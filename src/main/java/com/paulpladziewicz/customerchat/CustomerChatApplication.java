6e0665078bcaa878a3444e1b5bff7e0155c85a2d
paulpladziewicz <ppladziewicz@gmail.com>
2023-02-24 21:19:52 -0500

    initial commit

---
+++ src/main/java/com/paulpladziewicz/customerchat/CustomerChatApplication.java
@@ -0,0 +1,13 @@
+package com.paulpladziewicz.customerchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paulpladziewicz/customerchat/CustomerChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulpladziewicz.customerchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
